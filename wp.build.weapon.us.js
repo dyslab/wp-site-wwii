7a3bb3366e66098dbfcee43d5b6189d6e8fb57d5
dyslab <13926430883@139.com>
2019-04-07 23:09:00 +0700

    Add catalog of US 😪

---
+++ wp.build.weapon.us.js
@@ -0,0 +1,17 @@
+const fileIDs = [
+    {
+      id: 'include_all_css',
+      title: 'CSS文件集合'
+    },
+    {
+      id: 'us_army',
+      title: '陆战武器'
+    },
+    {
+      id: 'us_shortcut',
+      title: '快捷目录'
+    }
+  ];
+  
+  module.exports = fileIDs;
+  
