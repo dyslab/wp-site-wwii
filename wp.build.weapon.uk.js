dcc8c5fb8e45970fb17859c158fa5c621a846a22
dyslab <45236395+dyslab@users.noreply.github.com>
2019-05-17 03:38:18 +0800

    Add content: tank MK :gb:

---
+++ wp.build.weapon.uk.js
@@ -0,0 +1,20 @@
+const fileIDs = [
+    {
+      id: 'include_all_css',
+      title: 'CSS文件集合'
+    },
+    {
+      id: 'uk_army_tankmk',
+      title: 'M2轻型/中型坦克'
+    },
+    {
+      id: 'uk_army',
+      title: '陆战武器'
+    },
+    {
+      id: 'uk_shortcut',
+      title: '快捷目录'
+    }
+  ];
+
+  module.exports = fileIDs;
