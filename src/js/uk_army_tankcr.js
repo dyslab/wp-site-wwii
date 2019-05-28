aac3163c5777ea44c987441abfeb55f64ff5a5ac
dyslab <45236395+dyslab@users.noreply.github.com>
2019-05-28 21:05:53 +0800

    Add content: British crusader tank ↗↗↗

---
+++ src/js/uk_army_tankcr.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import BaseIO from './base.js';
+import footerJSON from './footer.json';
+import menuJSON from './uk_menu.json';
+import template from '../views/document.pug';
+
+const pageID = 'uk_army_tankcr';
+var bodyJSON = require(`./${pageID}.json`);
+
+// Set header and footer
+BaseIO.setCopyright(footerJSON);
+BaseIO.setMenuActiveItem(menuJSON, pageID);
+
+// Render pug file and output.
+document.write(template({
+  body: bodyJSON,
+  footer: footerJSON,
+  menu: menuJSON
+}));
--- wp.build.weapon.uk.js
+++ wp.build.weapon.uk.js
@@ -24,6 +24,10 @@ const fileIDs = [
       title: 'A9/A10/A13巡航坦克'
     },
     {
+      id: 'uk_army_tankcr',
+      title: 'A15“十字军”巡航坦克'
+    },
+    {
       id: 'uk_army',
       title: '陆战武器'
     },
