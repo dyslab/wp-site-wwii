18efcbb26f4b01e939d9635dbf37a27b69268d3b
dyslab <45236395+dyslab@users.noreply.github.com>
2019-06-07 16:12:17 +0800

    Add content: Japanese tank 94/95 👓

--- wp.build.weapon.jp.js
+++ wp.build.weapon.jp.js
@@ -4,6 +4,14 @@ const fileIDs = [
       title: 'CSS文件集合'
     },
     {
+      id: 'jp_army_tank94',
+      title: '94式小型坦克'
+    },
+    {
+      id: 'jp_army_tank95',
+      title: '95式轻型坦克'
+    },
+    {
       id: 'jp_army',
       title: '陆战武器'
     },
