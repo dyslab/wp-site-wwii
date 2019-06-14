3c86495ed7790ad55be35eb67778bfb3b37ce6ab
dyslab <45236395+dyslab@users.noreply.github.com>
2019-06-15 05:41:03 +0800

    🇫🇷 Add content: French tank 🇫🇷

--- wp.build.fr.js
+++ wp.build.fr.js
@@ -6,6 +6,16 @@ const fileIDs = [
   },
   {
     folder: 'js_fr',
+    id: 'fr_army_tanklt',
+    title: '法国轻型坦克'
+  },
+  {
+    folder: 'js_fr',
+    id: 'fr_army_tankmh',
+    title: '法国中型与重型坦克'
+  },
+  {
+    folder: 'js_fr',
     id: 'fr_shortcut',
     title: '快捷目录'
   }
