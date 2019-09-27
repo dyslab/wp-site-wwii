fa4fc1a16bfa3dceec2bcbf4a5ca04235bef3296
dyslab <45236395+dyslab@users.noreply.github.com>
2019-09-28 06:17:31 +0800

    Add content: DE SP Gun III ⚡

--- wp.build.de.js
+++ wp.build.de.js
@@ -86,6 +86,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_spgunIII',
+    title: 'Ⅲ号突击炮车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
