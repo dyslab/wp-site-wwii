336043f6429d725d7c41573a06ba88e670b941ee
dyslab <45236395+dyslab@users.noreply.github.com>
2019-10-05 01:12:34 +0800

    Add content: DE SP Gun Hummel 🌇

--- wp.build.de.js
+++ wp.build.de.js
@@ -96,6 +96,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_spgunHummel',
+    title: '“野蜂”自行榴弹炮车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
