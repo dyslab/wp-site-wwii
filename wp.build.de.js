7935a4021dd4b77122e871f535fc2885b8535e6a
dyslab <45236395+dyslab@users.noreply.github.com>
2019-10-07 18:48:55 +0800

    Add content: DE SP Gun Brummbar. 🖍

--- wp.build.de.js
+++ wp.build.de.js
@@ -101,6 +101,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_spgunBrummbar',
+    title: '“灰熊”自行突击炮车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
