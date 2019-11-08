67145097f09abd4efb57d2a9eb005d646d90bbc5
dyslab <45236395+dyslab@users.noreply.github.com>
2019-11-09 05:02:01 +0800

    Add content: DE AMV 251/22. 💤

--- wp.build.de.js
+++ wp.build.de.js
@@ -171,6 +171,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_avhtv25122',
+    title: '251/22型半履带装甲炮车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
