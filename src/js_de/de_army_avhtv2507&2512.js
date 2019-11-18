ef67b6c8fc72a7dcfa62a2413238747dac9629f4
dyslab <13926430883@139.com>
2019-11-19 04:23:24 +0800

    Add content: DE AMV 250/7 & 251/2 😊

---
+++ src/js_de/de_army_avhtv2507&2512.js
@@ -0,0 +1,7 @@
+import OutputHtml from '../js_common_base/common_weapon.js';
+
+OutputHtml(
+    'de', 'army',
+    require('./de_army_avhtv2507&2512.json'),
+    require('./de_menu.json')
+);
--- wp.build.de.js
+++ wp.build.de.js
@@ -186,6 +186,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_avhtv2507&2512',
+    title: '250/7、251/2型半履带装甲迫击炮车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
