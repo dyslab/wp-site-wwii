d16e1919153f6af34f7bc07cdbc5fa93f435ac96
dyslab <13926430883@139.com>
2019-12-25 05:12:18 +0800

    Add content: DE AMV 251/4 🕯️

--- wp.build.de.js
+++ wp.build.de.js
@@ -256,6 +256,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_avhtv2514',
+    title: '251/4型半履带装甲火炮牵引（弹药运载）车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
