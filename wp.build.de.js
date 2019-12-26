5702c9629c20c7f536831de12938b50eda990ceb
dyslab <13926430883@139.com>
2019-12-27 02:49:11 +0800

    Add content: DE AMV 251/16 🔥

--- wp.build.de.js
+++ wp.build.de.js
@@ -261,6 +261,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_avhtv25116',
+    title: '251/16型半履带装甲喷火车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
