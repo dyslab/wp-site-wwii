eaf13646dcdb8d626256eece738cb8a2b571d9c6
dyslab <13926430883@139.com>
2020-01-07 23:29:24 +0800

    Add content: DE AMV Sdkfz 11 🙇🏻

--- wp.build.de.js
+++ wp.build.de.js
@@ -286,6 +286,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_sdkfz11',
+    title: 'Sdkfz 11型半履带车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
