93bad61d9d5927e92c6deb4ef2d84e9248688391
vincento <vincent_to@qq.com>
2020-07-24 02:15:07 +0700

    Add content: DE MT Opel 🚛

--- wp.build.de.js
+++ wp.build.de.js
@@ -341,6 +341,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_mtOpel',
+    title: '欧宝（闪电）军用卡车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
