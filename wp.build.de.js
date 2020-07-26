6e6b676262567c6c57f51bd877a9f8944deb7450
vincento <vincent_to@qq.com>
2020-07-27 03:02:43 +0700

    Add content: DE MT Ford

--- wp.build.de.js
+++ wp.build.de.js
@@ -351,6 +351,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_mtFord',
+    title: '福特军用卡车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
