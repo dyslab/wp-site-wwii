e5c5b5d550c7096e6dfe404f9734ac318fe1660e
vincento <vincent_to@qq.com>
2020-07-29 03:14:36 +0700

    🚙 Add content: DE MT Other 🚙

--- wp.build.de.js
+++ wp.build.de.js
@@ -361,6 +361,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_mtsVW',
+    title: '大众军用小车'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
