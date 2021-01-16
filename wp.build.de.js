a80b6a98df3e89f7164d60a97d65e33770d0ccf2
vincento <vincent_to@qq.com>
2021-01-17 02:49:03 +0700

    Add: DE Flamethrower

--- wp.build.de.js
+++ wp.build.de.js
@@ -516,6 +516,11 @@ const fileIDs = [
   },
   {
     folder: 'js_de',
+    id: 'de_army_iwFlamethrower',
+    title: '德军火焰喷射器'
+  },
+  {
+    folder: 'js_de',
     id: 'de_airforce',
     title: '空战武器'
   },
